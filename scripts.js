4ccc3cc5db0c6b2f07360f15416000e58759c1b7
Nicolas Torres <desmondgamesyt@gmail.com>
2023-01-19 14:04:49 -0300

    i added functions to make the button 'know more' show the paragraph content

---
+++ scripts.js
@@ -0,0 +1,46 @@
+document.getElementById('btn-more1').addEventListener('click', function (event) {
+    if(document.getElementById('firstpage').style.display == 'none' || document.getElementById('firstpage').style.display == ''){
+        document.getElementById('firstpage').style.display = 'block';
+    }else{
+        document.getElementById('firstpage').style.display = 'none';
+    }
+        });
+document.getElementById('btn-more2').addEventListener('click', function (event){
+    if(document.getElementById('dashmoto').style.display =='none' || document.getElementById('dashmoto').style.display == ''){
+        document.getElementById('dashmoto').style.display = 'block';
+        }else{
+            document.getElementById('dashmoto').style.display = 'none';
+        }
+});
+
+document.getElementById('btn-more3').addEventListener('click', function (event){
+    if(document.getElementById('aboutmepro1').style.display == 'none' || document.getElementById('aboutmepro1').style.display == ''){
+        document.getElementById('aboutmepro1').style.display = 'block';
+    }else{
+        document.getElementById('aboutmepro1').style.display = 'none';
+    }
+})
+document.getElementById('btn-more4').addEventListener('click', function (event){
+    if(document.getElementById('company').style.display == 'none' || document.getElementById('company').style.display == ''){
+        document.getElementById('company').style.display = 'block';
+    }else{
+        document.getElementById('company').style.display = 'none';
+    }
+})
+document.getElementById('btn-more5').addEventListener('click', function (event){
+    if(document.getElementById('ong').style.display == 'none' || document.getElementById('ong').style.display == ''){
+        document.getElementById('ong').style.display = 'block';
+    }else{
+        document.getElementById('ong').style.display = 'none';
+    }
+})
+document.getElementById('btn-more6').addEventListener('click', function (event){
+    if(document.getElementById('aboutmepro2').style.display == 'none' || document.getElementById('aboutmepro2').style.display == ''){
+        document.getElementById('aboutmepro2').style.display = 'block';
+    }else{
+        document.getElementById('aboutmepro2').style.display = 'none';
+    }
+})
+        
+
+
